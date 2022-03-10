af12eb976f7193600a4ceb5260492b9282f306d8
Mohanraj Muthukumaran <mohanrajmuthukumaran@Mohanrajs-MacBook-Pro.local>
2022-03-10 12:16:19 +0530

    [mohanraj/samvaradhan] Added test to check invalid amount

--- src/test/java/com/tw/wallet/WalletTest.java
+++ src/test/java/com/tw/wallet/WalletTest.java
@@ -5,10 +5,16 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class WalletTest {
 
     Wallet wallet = new Wallet();
+
+    @Test
+    public void shouldNotAbleToCreateRupeeIfValueIsInvalid(){
+        assertThrows(InvalidValueException.class, () -> new Rupee(-1));
+    }
 
     @Test
     public void shouldAdd15RupeesInWallet() throws InvalidValueException {
